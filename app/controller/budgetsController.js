c5e6473c22fad38caf8e8fb477c2584d2508d6c8
aldrinlijo <aldrinlijo1996@gmail.com>
2023-11-29 18:37:41 +0530

    first  commit

--- app/controller/budgetsController.js
+++ app/controller/budgetsController.js
@@ -6,7 +6,7 @@ budgetController.update = (req, res) => {
     const id = req.params.id
     const user = req.tokenData._id
     const body = req.body
-    const add  = body + 10000
+    const add  = 10000
     Budget.findOneAndUpdate({id, user}, add, {new:true})
         .then((budget) => {
             res.json(budget)
